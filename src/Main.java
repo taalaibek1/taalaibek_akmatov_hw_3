2462f361f96af53e707d5b301603dd0067f5ff8f
taalaibek1 <87862312+taalaibek1@users.noreply.github.com>
2024-06-12 12:14:14 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,50 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        float[] array = {3, 93, 4,2,-3,4,-35,-87,2,5545,-3,24,1,5,7};
-        boolean negativeFound = false;
 
-        float quantityOfPositiveNumbers=0;
-        float sum=0;
-        
+    
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write 5 names: ");
+        ArrayList<String> namesA = new ArrayList<>();
+        ArrayList<String> namesB = new ArrayList<>();
+        ArrayList<String> namesC = new ArrayList<>();
 
 
-        for(float currentNumber : array){
-            if(currentNumber<0){
-                negativeFound=true;
-            }else if(negativeFound){
-                quantityOfPositiveNumbers=quantityOfPositiveNumbers+1;
-                sum=sum+currentNumber;
-            }
 
+        for(int i=0; i<5; i++){
+            String scanA = scanner.next();
+            System.out.println("You have written names for list A : " + scanA );
+            namesA.add(scanA);
 
         }
+        System.out.println("First list:" + "\n" +namesA);
 
-        System.out.println(sum/quantityOfPositiveNumbers);
+
+        for(int i = 0;i<5; i++){
+            String scanB = scanner.next();
+            System.out.println("You have written names for list B : " + scanB );
+            namesB.add(scanB);
+        }
+        System.out.println("Second list:" + "\n" +namesB);
+
+namesC.add(namesA.get(0));
+namesC.add(namesB.get(4));
+        namesC.add(namesA.get(1));
+        namesC.add(namesB.get(3));
+        namesC.add(namesA.get(2));
+        namesC.add(namesB.get(2));
+        namesC.add(namesA.get(3));
+        namesC.add(namesB.get(1));
+        namesC.add(namesA.get(4));
+        namesC.add(namesB.get(0));
+
+        System.out.println("Third List:\n"+namesC);
+
+        namesC.sort(Comparator.comparing(String::length));
+        System.out.println("Sorted list:\n" + namesC);
 
 
     }
